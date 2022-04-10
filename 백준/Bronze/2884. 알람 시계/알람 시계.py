d6a40cce41af7978acf2b004ccaf20898130e48a
JunhyeongChoi <103200144+JunhyeongChoi@users.noreply.github.com>
2022-04-10 13:22:18 +0900

    [Bronze III] Title: 알람 시계, Time: 116 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -0,0 +1,10 @@
+import sys
+
+hour, minute = map(int, sys.stdin.readline().split())
+
+if minute >= 45:
+    print(hour, minute-45)
+elif hour == 0 and minute < 45:
+    print(23, minute+15)
+elif hour != 0 and minute < 45:
+    print(hour-1, minute+15)
