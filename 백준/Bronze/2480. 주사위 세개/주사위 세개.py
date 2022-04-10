9d666856cd1c2420fa8ed7b00ed59e14d3402d17
JunhyeongChoi <103200144+JunhyeongChoi@users.noreply.github.com>
2022-04-10 14:42:47 +0900

    [Bronze IV] Title: 주사위 세개, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,29 +1,12 @@
 import sys
 
 a, b, c = map(int, sys.stdin.readline().split())
-same = 0
-amount = 1
-max = 0
 
 if a == b == c:
-    amount = 3
-elif a == b or a == c:
-    same = a
-    amount = 2
-elif b == c:
-    same = b
-    amount = 2
-
-if a > max:
-    max = a
-if b > max:
-    max = b
-if c > max:
-    max = c
-
-if amount == 3:
     print(10000 + a * 1000)
-elif amount == 2:
-    print(1000 + same * 100)
-elif amount == 1:
-    print(max * 100)
+elif a == b or a == c:
+    print(1000 + a * 100)
+elif b == c:
+    print(1000 + b * 100)
+else:
+    print(max(a, b, c) * 100)
