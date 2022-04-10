af74fbbb56f78456ddbeb28b1896a578c5ee7807
JunhyeongChoi <103200144+JunhyeongChoi@users.noreply.github.com>
2022-04-10 19:35:53 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 120 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -3,14 +3,11 @@ import sys
 c = int(sys.stdin.readline())
 
 for _ in range(c):
-    sum = 0
     amount = 0
     lst = list(map(int, sys.stdin.readline().split()))
-    for i in range(1, len(lst)):
-        sum += lst[i]
-    average = sum / lst[0]
-    for i in range(1, len(lst)):
-        if lst[i] > average:
+    average = sum(lst[1:]) / lst[0]
+    for i in lst[1:]:
+        if i > average:
             amount += 1
     result = amount / lst[0] * 100
     print(f"{result:0.3f}%")
