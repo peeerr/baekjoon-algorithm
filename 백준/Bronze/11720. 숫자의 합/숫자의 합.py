111c02b64ea3b53e632912d085c3319591aa4549
JunhyeongChoi <103200144+JunhyeongChoi@users.noreply.github.com>
2022-04-11 12:56:57 +0900

    [Bronze II] Title: 숫자의 합, Time: 104 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.py
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.py
@@ -1,9 +1,4 @@
 import sys
 
 n = sys.stdin.readline()
-num = sys.stdin.readline().strip()
-sum = 0
-
-for i in num:
-    sum += int(i)
-print(sum)
+print(sum(map(int, sys.stdin.readline().strip())))
