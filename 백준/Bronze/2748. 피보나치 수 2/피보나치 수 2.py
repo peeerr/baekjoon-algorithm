761f7afdf4a148ec1b6269096ba180815ccf5300
peeerr <ajfeh22@gmail.com>
2023-03-24 22:42:49 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,15 +1,12 @@
 import sys
 
 n = int(sys.stdin.readline())
-d = [0] * n
+d = [0] * (n + 1)
 
-if n == 1 or n == 2:
-    print(1)
-else:
-    d[0] = 1
-    d[1] = 1
+d[0] = 1
+d[1] = 1
 
-    for i in range(2, n):
-        d[i] = d[i - 1] + d[i - 2]
+for i in range(2, n):
+    d[i] = d[i - 1] + d[i - 2]
 
-    print(d[n - 1])
+print(d[n - 1])
