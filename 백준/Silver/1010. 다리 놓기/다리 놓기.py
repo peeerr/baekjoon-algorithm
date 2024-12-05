2e80f6692ac90d95e906431cd3821257c9927acc
peeerr <ajfeh22@gmail.com>
2024-12-05 14:34:50 +0900

    [Silver V] Title: 다리 놓기, Time: 128 ms, Memory: 110404 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,15 +1,17 @@
+def factorial(n, cnt):
+    ans = 1
+    for i in range(cnt):
+        ans *= n
+        n -= 1
+    return ans
+
+
 t = int(input())
 
 for _ in range(t):
     west_cnt, east_cnt = map(int, input().split())
 
-    dp = [[0 for _ in range(west_cnt + 1)] for _ in range(east_cnt + 1)]
+    top = factorial(east_cnt, west_cnt)
+    bottom = factorial(west_cnt, west_cnt)
 
-    for i in range(1, east_cnt + 1):
-        dp[i][1] = i
-
-    for i in range(2, east_cnt + 1):
-        for j in range(2, west_cnt + 1):
-            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
-
-    print(dp[east_cnt][west_cnt])
+    print(top // bottom)
