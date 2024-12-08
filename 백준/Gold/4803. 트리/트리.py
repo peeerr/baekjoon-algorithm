70dccfd776abc34c12346a81f18aff94fbb0634e
peeerr <ajfeh22@gmail.com>
2024-12-09 01:01:37 +0900

    [Gold IV] Title: 트리, Time: 204 ms, Memory: 116772 KB -BaekjoonHub

---
+++ 백준/Gold/4803. 트리/트리.py
@@ -0,0 +1,46 @@
+def dfs(start):
+    global edge_cnt, v_cnt
+    for v in graph[start]:
+        edge_cnt += 1
+        if not visited[v]:
+            visited[v] = True
+            v_cnt += 1
+            dfs(v)
+
+
+case = 1
+
+while True:
+    n, m = map(int, input().split())
+
+    if n == 0 and m == 0:
+        break
+
+    graph = [[] for _ in range(n + 1)]
+
+    for _ in range(m):
+        u, v, = map(int, input().split())
+        graph[u].append(v)
+        graph[v].append(u)
+
+    visited = [False for _ in range(n + 1)]
+    ans = 0
+
+    for i in range(1, n + 1):
+        if visited[i]:
+            continue
+
+        edge_cnt, v_cnt = 0, 1
+        visited[i] = True
+        dfs(i)
+
+        if v_cnt - 1 == edge_cnt // 2:
+            ans += 1
+
+    if ans > 1:
+        print(f"Case {case}: A forest of {ans} trees.")
+    elif ans == 1:
+        print(f"Case {case}: There is one tree.")
+    else:
+        print(f"Case {case}: No trees.")
+    case += 1
