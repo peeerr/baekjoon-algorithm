a59480cd352ec50cd622228cd8308d76da7d93f5
peeerr <ajfeh22@gmail.com>
2024-12-22 20:48:23 +0900

    [Silver I] Title: 안전 영역, Time: 736 ms, Memory: 228988 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -0,0 +1,60 @@
+import sys
+
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+temp = [[0 for _ in range(n)] for _ in range(n)]
+visited = [[False for _ in range(n)] for _ in range(n)]
+
+
+def init():
+    for i in range(n):
+        for j in range(n):
+            temp[i][j] = grid[i][j]
+            visited[i][j] = False
+
+
+def submerge(height):
+    for i in range(n):
+        for j in range(n):
+            if temp[i][j] <= height:
+                temp[i][j] = 0
+
+
+def can_go(x, y):
+    return 0 <= x < n and 0 <= y < n and not visited[x][y] and temp[x][y]
+
+
+def dfs(x, y):
+    dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if can_go(nx, ny):
+            visited[nx][ny] = True
+            dfs(nx, ny)
+
+
+def find_cnt():
+    cnt = 0
+    for i in range(n):
+        for j in range(n):
+            if not visited[i][j] and temp[i][j]:
+                visited[i][j] = True
+                dfs(i, j)
+                cnt += 1
+    return cnt
+
+
+def simulate():
+    ans = 0
+    for height in range(101):
+        init()
+        submerge(height)
+        ans = max(ans, find_cnt())
+    return ans
+
+
+print(simulate())
