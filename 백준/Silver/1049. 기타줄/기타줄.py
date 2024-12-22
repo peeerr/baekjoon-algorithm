d51e8c9d2187247a1ee4ddb804e3b680c74d06c8
peeerr <ajfeh22@gmail.com>
2024-12-22 18:52:44 +0900

    [Silver IV] Title: 기타줄, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.py
+++ 백준/Silver/1049. 기타줄/기타줄.py
@@ -10,6 +10,9 @@ for _ in range(m):
 packages.sort()
 strings.sort()
 
+# 1. 낱개만 사용했을 때
+# 2. 패키지만 사용했을 때
+# 3. 패키지 사용하고 남은 개수는 낱개를 사용할 때
 ans1 = n * strings[0]
 ans2 = n // 6 * packages[0] + packages[0]
 ans3 = n // 6 * packages[0] + (n % 6 * strings[0])
