5b1e2bb122af65a2af1550072e8d56d7e5dbbce9
peeerr <ajfeh22@gmail.com>
2024-12-27 01:29:01 +0900

    [Gold V] Title: 퇴사 2, Time: 2460 ms, Memory: 254076 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,15 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+
 n = int(input())
-t, p = [0 for _ in range(n + 1)], [0 for _ in range(n + 1)]
-for i in range(1, n + 1):
-    t[i], p[i] = map(int, input().split())
+info = [0] + [tuple(map(int, input().split())) for _ in range(n)]
 
 dp = [0 for _ in range(n + 1)]
+max_p = 0  # 현재까지의 최대 수익
 
 for i in range(1, n + 1):
-    dp[i] = max(dp[i], dp[i - 1])
-    
-    date = i + t[i] - 1
-    if date <= n:
-        dp[date] = max(dp[date], dp[i - 1] + p[i])
-        
+    # 이전까지의 최대 수익 갱신
+    max_p = max(max_p, dp[i - 1])
+
+    t, p = info[i]
+    if i + t > n + 1:
+        continue
+
+    # i일에 상담을 하는 경우의 수익 = 현재까지 최대 수익 + 현재 상담 수익
+    dp[i + t - 1] = max(dp[i + t - 1], max_p + p)
+
 print(max(dp))
